234072f33236dde433ed167611cac3302f3ab9a4
Sergey <zapravilsj@mail.ru>
2022-08-03 22:53:10 +0500

    Добавил шестую задачу

--- src/Main.java
+++ src/Main.java
@@ -17,10 +17,18 @@ public class Main {
         System.out.println("Задача 3");
         int deliveryDistance = 95;
         System.out.println("Потребуется дней: " + calculateTheDeliveryDate(deliveryDistance));
+
+        // Задача 6
+        System.out.println("Задача 6");
+        int[] accountantsBook = generateRandomArray();
+        System.out.print("Траты по дням: ");
+        printArray(accountantsBook);
+        System.out.println("Сумма трат за месяц составила " + theSumOfAllExpensesForTheMonth(accountantsBook) + " рублей.");
+        System.out.println("Средняя сумма трат за месяц составила " + calculationOfAverageSpendingPerDay(theSumOfAllExpensesForTheMonth(accountantsBook), accountantsBook.length) + " рублей.");
     }
 
     public static void definesALeapYear(int a) {
-        boolean visokosniy = ((a % 4 == 0) && (a % 100 !=  0) || (a % 400 == 0));
+        boolean visokosniy = ((a % 4 == 0) && (a % 100 != 0) || (a % 400 == 0));
         if (visokosniy) {
             System.out.println(a + " год является високосным");
         } else
@@ -54,5 +62,36 @@ public class Main {
             dayOfDelivery++;
         }
         return dayOfDelivery;
+    }
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void printArray(int[] accountantsBook) {
+        for (int i = 0; i < accountantsBook.length; i++) {
+            if (i == accountantsBook.length - 1) {
+                System.out.println(accountantsBook[i]);
+            } else {
+                System.out.print(accountantsBook[i] + ", ");
+            }
+        }
+    }
+
+    public static int theSumOfAllExpensesForTheMonth(int[] accountantsBook) {
+        int sum = accountantsBook[0];
+        for (int i = 0; i < accountantsBook.length; i++) {
+            sum = sum + accountantsBook[i];
+        }
+        return sum;
+    }
+    public static double calculationOfAverageSpendingPerDay(int sum, int quantity) {
+        return (double) sum / quantity;
     }
 }
